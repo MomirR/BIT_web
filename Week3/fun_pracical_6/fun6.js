2ff78c97cb1b756f6c29fec9738155b0813a1158
BIT Student <classroom@bgit.rs>
2019-05-10 18:01:14 +0200

    sass-bootstrap

--- Week3/fun_pracical_6/fun6.js
+++
@@ -1,4 +0,0 @@
-// Write a program that checks if a given array is symmetric.
-// An array is symmetric if it can be read the same way both from the left and the right hand side.   
-// Input array: [2, 4, -2, 7, -2, 4, 2]
-// Output: The array is symmetric.
