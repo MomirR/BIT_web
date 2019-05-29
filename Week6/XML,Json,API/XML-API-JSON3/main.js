702e468a8f1b507065453c02998080b1b42cc925
Momir Radovanovic <momirradovanovic@yahoo.com>
2019-05-29 23:25:35 +0200

    GIT-API

--- Week6/XML,Json,API/XML-API-JSON3/main.js
+++ Week6/XML,Json,API/XML-API-JSON3/main.js
@@ -1,4 +1,3 @@
-// The user can input some text in the search box
 // When the user clicks enter, a request is sent using jQuery Ajax API
 
 // The request is sent to GitHub’s endpoint for searching users
@@ -6,3 +5,36 @@
 // Try to find URL and structure of a request for searching users
 
 // When a response is received, print an image and a username on the page for all the users in the response
+
+var $input = document.querySelector("#input");
+var $searchButton = document.querySelector("#search");
+var $img = document.querySelector("#img");
+
+
+$searchButton.addEventListener("click", pullingImagesAndUsers);
+
+function pullingImagesAndUsers() {
+    var request = new XMLHttpRequest;
+    var inputValue = $input.value;
+    var inputValueArray = inputValue.split(" ");
+    request.open('GET', 'https://api.github.com/search/users?q=' + inputValue + '_1+SEARCH_KEYWORD_N+QUALIFIER_1+QUALIFIER_N' + , true);
+    // var parsedRequest = JSON.parse(requestValue);
+    request.send();
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText);
+
+            for (var i = 0, j = 1; i < data.length; i++ , j++) {
+                document.querySelector("#img" + j).setAttribute("src", data[i].avatar_url); //Dynamic $Variable and URL      
+                if (j === 6) {
+                    break;
+                }
+            }
+            console.log(data);
+        } else {
+            return "Error";
+        }
+    }
+    // console.log(data);
+}
