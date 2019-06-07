ae91da787105b3b24f890e8aab40c58589158993
BIT Student <classroom@bgit.rs>
2019-06-07 17:47:02 +0200

    ecma6

--- Week7/practical_ECMA6/ecma6_4.js
+++ Week7/practical_ECMA6/ecma6_4.js
@@ -1,3 +1,11 @@
 // Write a function that filters all even elements of the array.
 // 	Input: [6, 11, 9, 0, 3]
 // 	Output: [6, 0]
+
+const numbers = [6, 11, 9, 0, 3];
+
+const filterEven = (num) => {
+    const res = num.filter()
+}
+
+filterEven(numbers);
